3983dc6f5eb5d350918f77a96daf564695c4a53e
shahrukh0526 <shahrukh0526@gmail.com>
2021-01-14 19:06:01 +0900

    プレイヤー・敵などの動きの関数を実装

--- Sakuhin.py
+++ Sakuhin.py
@@ -9,7 +9,7 @@ BOX_HEIGHT = 400       # ゲーム領域の高さ
 PLAYER_X = 0  # プレイヤーのx方向の位置
 PLAYER_VY = 20  # プレイヤーのy方向の速度
 PLAYER_HP = 500  # プレイヤーのHP
-KIDAN_VX = 10  # 気弾の速さ
+KIDAN_VX = -10  # 気弾の速さ
 KIDAN_POWER = 50  # 気弾のパワー
 ENEMY_HP1 = 100  # 強さが1の敵のHP
 ENEMY_HP2 = 200  # 強さが2の敵のHP
@@ -20,56 +20,82 @@ ENEMY_VX3 = 40  # 強さが3の敵の速さ
 ENEMY_POWER1 = 100  # 強さが1の敵のパワー
 ENEMY_POWER2 = 200  # 強さが2の敵のパワー
 ENEMY_POWER3 = 300  # 強さが3の敵のパワー
-SCORE1 = 100  # 強さが1の敵を倒したときのスコア
-SCORE2 = 300  # 強さが2の敵を倒したときのスコア
-SCORE3 = 500  # 強さが3の敵を倒したときのスコア
+ENEMY_SCORE1 = 100  # 強さが1の敵を倒したときのスコア
+ENEMY_SCORE2 = 300  # 強さが2の敵を倒したときのスコア
+ENEMY_SCORE3 = 500  # 強さが3の敵を倒したときのスコア
 
 # プレイヤーのクラス
 
 
 class Player(pygame.sprite.Sprite):  # Sprite継承
-    def __init__(self, screen, x, y, vy):  # コンストラクタ
+    def __init__(self, screen, x, y):  # コンストラクタ
         super().__init__()  # ←この一行
         self.screen = screen
         self.vy = PLAYER_VY
+        self.hp = PLAYER_HP
         self.rect = pygame.Rect(x, y, 130, 150)
         self.image = pygame.image.load("player.png")  # プレイヤーの画像を読み込む
         self.image = self.image.convert()  # 画像を変換する
         self.d_y = 0
 
+    def update(self):  # 描画位置を移動させる
+        self.rect.move_ip(0, self.d_y)
+        self.d_y = 0
+
+    def up(self):  # プレイヤーに↑を押したときの処理
+        y = self.rect.y-self.d_y
+        if y >= 0:
+            self.d_y -= self.vy
+
+    def down(self):  # プレイヤーに↓を押したときの処理
+        self.d_y += self.vy
+
 # 敵のクラス
 
 
 class Enemy(pygame.sprite.Sprite):  # Sprite継承
-    def __init__(self, screen, x, y, vx, strength):  # コンストラクタ
+    def __init__(self, screen, x, y, strength):  # コンストラクタ
         super().__init__()  # ←この一行
         self.screen = screen
-        if strength == 1:
+        if strength == 1:  # 強さが1の時
             self.vx = ENEMY_VX1
+            self.hp = ENEMY_HP1
+            self.power = ENEMY_POWER1
+            self.score = ENEMY_SCORE1
             self.rect = pygame.Rect(x, y, 86, 86)
             self.image = pygame.image.load("zonbi.jpg")  # 強さ１の敵の画像を読み込む
             self.image = self.image.convert()  # 画像を変換する
-        if strength == 2:
+        if strength == 2:  # 強さが2の時
             self.vx = ENEMY_VX2
+            self.hp = ENEMY_HP2
+            self.power = ENEMY_POWER2
+            self.score = ENEMY_SCORE2
             self.rect = pygame.Rect(x, y, 86, 86)
-            self.image = pygame.image.load("zonbi.jpg")  # 戦車の画像を読み込む
+            self.image = pygame.image.load("reaper.jpg")  # 強さ2の敵の画像を読み込む
             self.image = self.image.convert()  # 画像を変換する
-        if strength == 1:
-            self.vx = ENEMY_VX1
+        if strength == 3:  # 強さが3の時
+            self.vx = ENEMY_VX3
+            self.hp = ENEMY_HP3
+            self.power = ENEMY_POWER3
+            self.score = ENEMY_SCORE3
             self.rect = pygame.Rect(x, y, 86, 86)
-            self.image = pygame.image.load("zonbi.jpg")  # 戦車の画像を読み込む
+            self.image = pygame.image.load("doragon.jpg")  # 強さ3の敵の画像を読み込む
             self.image = self.image.convert()  # 画像を変換する
+
+    def update(self):  # 描画位置を移動させる
+        self.rect.move_ip(self.vx, 0)
 
 # 気弾のクラス
 
 
 class Kidan(pygame.sprite.Sprite):  # Sprite継承
-    def __init__(self, screen, x, y, vx):  # コンストラクタ
+    def __init__(self, screen, x, y):  # コンストラクタ
         super().__init__()
         self.screen = screen
         self.vx = KIDAN_VX
-        self.rect = pygame.Rect(x, y, 45, 26)
-        self.image = pygame.image.load("bullet1.jpg")  # 弾丸の画像を読み込む
+        self.power = KIDAN_POWER
+        self.rect = pygame.Rect(x, y, 100, 66)
+        self.image = pygame.image.load("kidan.jpg")  # 気弾の画像を読み込む
         self.image = self.image.convert()  # 画像を変換する
 
     def update(self):  # 描画位置を移動させる
@@ -103,3 +129,12 @@ class Game:
         self.player = None
         self.enemy = None
         self.kidans = pygame.sprite.Group()  # Groupクラス
+        self.attack = None
+        self.hitpoint = None
+        self.special = None
+
+    def set(self):   # 初期設定を一括して行う
+        screen = pygame.display.set_mode((640, 400))  # pygameのディスプレイ生成
+        self.screen = screen
+        self.player = Player(screen, 100, 100)  # プレイヤーオブジェクト
+        self.spear = Spear(screen, 0, 200)  # 槍オブジェクト
