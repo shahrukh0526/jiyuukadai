c5688b2954dfbca512c6a5ad45d728d7171c0444
shahrukh0526 <shahrukh0526@gmail.com>
2021-01-16 22:00:19 +0900

    気弾・敵、プレイヤー・敵の衝突判定を実装

--- Sakuhin.py
+++ Sakuhin.py
@@ -27,7 +27,7 @@ ENEMY_SCORE1 = 100  # 強さが1の敵を倒したときのスコア
 ENEMY_SCORE2 = 300  # 強さが2の敵を倒したときのスコア
 ENEMY_SCORE3 = 500  # 強さが3の敵を倒したときのスコア
 ATTACK_COLOR = (255, 0, 0)  # 攻撃力UPのボタンの色
-HITPOINT_COLOR = (0, 0, 255)　  # 体力UPのボタンの色
+HITPOINT_COLOR = (0, 0, 255)  # 体力UPのボタンの色
 SPECIALATTACK_COLOR = (255, 255, 0)  # 特殊攻撃のボタンの色
 
 # プレイヤーのクラス
@@ -39,6 +39,7 @@ class Player(pygame.sprite.Sprite):  # Sprite継承
         self.screen = screen
         self.vy = PLAYER_VY
         self.hp = PLAYER_HP
+        self.power = KIDAN_POWER
         self.rect = pygame.Rect(x, y, 130, 150)
         self.image = pygame.image.load("player.png")  # プレイヤーの画像を読み込む
         self.image = self.image.convert()  # 画像を変換する
@@ -99,7 +100,6 @@ class Kidan(pygame.sprite.Sprite):  # Sprite継承
         super().__init__()
         self.screen = screen
         self.vx = KIDAN_VX
-        self.power = KIDAN_POWER
         self.rect = pygame.Rect(x, y, 100, 66)
         self.image = pygame.image.load("kidan.jpg")  # 気弾の画像を読み込む
         self.image = self.image.convert()  # 画像を変換する
@@ -157,25 +157,27 @@ class Game:
         self.width = w
         self.height = h
         self.player = None
-        self.enemys = pygame.sprite.Group()  # Groupクラス
+        self.enemys = []
         self.kidans = pygame.sprite.Group()  # Groupクラス
         self.attack = None
         self.hitpoint = None
         self.special = None
+        self.score = 0
         self.count = 0
 
     def set(self):   # 初期設定を一括して行う
         screen = pygame.display.set_mode((660, 400))  # pygameのディスプレイ生成
         self.screen = screen
+        self.clock = pygame.time.Clock()     # 時計オブジェクト
         self.player = Player(screen, 500, 100)  # プレイヤーオブジェクト
 
     def kidan_s(self):  # 気弾を生成
         self.kidans.add(Kidan(self.screen, self.player.rect.x,
                               self.player.rect.y))  # addメソッド
 
-    def gen_enemys(self, strength):  # 気弾を生成
-        self.enemys.add(
-            Enemy(self.screen, -10, random.randint(0, 300)), strength)  # addメソッド
+    def gen_enemys(self, strength):  # 敵を生成
+        self.enemys.append(
+            Enemy(self.screen, -10, random.randint(0, 300), strength))
 
     def animate(self):
         LOOP = True
@@ -185,45 +187,50 @@ class Game:
                 if event.type == pygame.QUIT:
                     LOOP = False
             self.clock.tick(FPS)
-            self.player.update()
-            self.kidans.update()
-            self.enemys.update()
             pressed_keys = pygame.key.get_pressed()  # キー情報を取得
-            collided1 = pygame.sprite.spritecollideany(
-                self.player, self.enemys)
-            collided2 = pygame.sprite.spritecollideany(
-                self.kidans, self.enemys)
             if pressed_keys[pygame.K_s]:    # sが押されたら
                 self.kidan_s()
-            if self.count <= 10:
-                time.sleep(1)
-                self.gen_enemys(1)
-            if self.count <= 20:
-                time.sleep(1)
-                self.gen_enemys(2)
-            if self.count > 20:
-                time.sleep(1)
-                self.gen_enemys(3)
-            if collided1:  # 衝突したら
-                self.enemys.remove()
+            time.sleep(1)
+            self.gen_enemys(random.randint(1, 3))
+            for enemy in self.enemys:
+                if pygame.sprite.collide_rect(enemy, self.player):  # 衝突判定
+                    if self.player.hp < enemy.power:
+                        break
+                    self.player.hp -= enemy.power
+                if pygame.sprite.spritecollideany(enemy, self.kidans):
+                    if enemy.hp < self.player.power:
+                        self.enemys.remove(enemy)
+                        self.count += 1
+                        self.score += enemy.score
+                    enemy.hp -= self.player.power
+            if self.count >= 6:
+                self.attack = Attack()
+                self.attack.draw()
+            # if self.count >= 6:
+            #     self.hitpoint=HitPoint()
+            #     self.hitpoint.draw()
+            # if self.count >= 12:
+            #     self.special=SpecialAttack()
+            #     self.special.draw()
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    print('i')
 
+            Score().draw(self.score)
+            self.player.update()
+            self.kidans.update()
             self.screen.blit(self.player.image, self.player.rect)
             self.kidans.draw(self.screen)
-            self.enemys.draw(self.screen)
-
-            if self.count > 50:  # 命中回数が50を超えたら
-                self.target1.update()
-                self.screen.blit(self.target1.image,
-                                 self.target1.rect)  # 新しい標的表示
-            if collided and self.count > 50:  # 命中回数が50を超え、標的と弾丸が衝突したら
-                self.screen.blit(self.spear.image, (30, 100))  # 槍表示
+            for enemy in self.enemys:
+                enemy.update()
+                self.screen.blit(enemy.image, enemy.rect)
             pygame.display.flip()
             self.screen.fill((0, 0, 0))
 
 
 # ----------------------------------
 # メインルーチン
-box = Box(BOX_WIDTH, BOX_HEIGHT)
-box.set()       # ゲームの初期設定
-box.animate()   # アニメーション
+game = Game(BOX_WIDTH, BOX_HEIGHT)
+game.set()       # ゲームの初期設定
+game.animate()   # アニメーション
 pygame.quit()  # 画面を閉じる
